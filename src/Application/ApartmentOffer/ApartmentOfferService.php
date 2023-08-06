6e2b43d723395257f38b62b4514d4e7b7f7ad818
Tomasz Ziebura <dev.ziebura@gmail.com>
2023-08-06 11:14:37 +0200

    Added test for ApartmentOfferService - TDD Refactor - added builder for offer

--- src/Application/ApartmentOffer/ApartmentOfferService.php
+++ src/Application/ApartmentOffer/ApartmentOfferService.php
@@ -4,6 +4,7 @@ namespace App\Application\ApartmentOffer;
 
 use App\Domain\ApartmentOffer\ApartmentAvailability;
 use App\Domain\ApartmentOffer\ApartmentOffer;
+use App\Domain\ApartmentOffer\ApartmentOfferBuilder;
 use App\Domain\ApartmentOffer\ApartmentOfferRepository;
 use App\Domain\ApartmentOffer\Money;
 use DateTimeImmutable;
@@ -19,11 +20,11 @@ class ApartmentOfferService
 
     public function add(string $apartmentId, float $price, DateTimeImmutable $start, DateTimeImmutable $end)
     {
-        $offer = new ApartmentOffer(
-            $apartmentId,
-            new Money($price),
-            new ApartmentAvailability($start, $end)
-        );
+        $offer = ApartmentOfferBuilder::create()
+            ->withApartmentId($apartmentId)
+            ->withPrice($price)
+            ->withAvailability($start, $end)
+            ->build();
 
         $this->apartmentOfferRepository->save($offer);
     }
---
+++ src/Domain/ApartmentOffer/ApartmentOfferBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Domain\ApartmentOffer;
+
+use DateTimeImmutable;
+use stdClass;
+
+class ApartmentOfferBuilder
+{
+    private stdClass $carry;
+
+    private function __construct()
+    {
+        $this->carry = new stdClass();
+    }
+
+    public static function create(): self
+    {
+        return new self();
+    }
+
+    public function withApartmentId(string $apartmentId): self
+    {
+        $this->carry->apartmentId = $apartmentId;
+        return $this;
+    }
+
+    public function withPrice(float $price): self
+    {
+        $this->carry->price = $price;
+        return $this;
+    }
+
+    public function withAvailability(DateTimeImmutable $start, DateTimeImmutable $end): self
+    {
+        $this->carry->start = $start;
+        $this->carry->end = $end;
+        return $this;
+    }
+
+    public function build(): ApartmentOffer
+    {
+        $offer = new ApartmentOffer(
+            $this->carry->apartmentId,
+            new Money($this->carry->price),
+            new ApartmentAvailability(
+                $this->carry->start,
+                $this->carry->end
+            )
+        );
+
+        $this->carry = new stdClass();
+        return $offer;
+    }
+}
