4347b3b57e5353bfb5eb491b4bb2abecfbf0fd01
Tomasz Ziebura <dev.ziebura@gmail.com>
2023-08-06 10:49:39 +0200

    Added test for ApartmentOfferService - TDD Red - added first scenario which tests if ApartmentOffer is saved

---
+++ src/Application/ApartmentOffer/ApartmentOfferService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\ApartmentOffer;
+
+use DateTimeImmutable;
+
+class ApartmentOfferService
+{
+    public function add(string $apartmentId, float $price, DateTimeImmutable $start, DateTimeImmutable $end)
+    {
+    }
+}
